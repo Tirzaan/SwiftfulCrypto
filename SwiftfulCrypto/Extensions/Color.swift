5c36e76cdacedf14bc67d8a309ee0347b13a6dbf
Tirzaan Neil <Tirzaan@tntneil.com>
2025-08-25 12:54:10 -0600

    [Episode] 1 - Setup

---
+++ SwiftfulCrypto/Extensions/Color.swift
@@ -0,0 +1,25 @@
+//
+//  Color.swift
+//  SwiftfulCrypto
+//
+//  Created by Tirzaan on 8/25/25.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    
+    static let theme = ColorTheme()
+    
+}
+
+struct ColorTheme {
+    
+    let accent = Color("AccentColor")
+    let background = Color("BackgroundColor")
+    let green = Color("GreenColor")
+    let red = Color("RedColor")
+    let secondaryText = Color("SecondaryTextColor")
+    
+}
