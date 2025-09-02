0d85143ce339cfc44cfdaff986b92a03c5faf66e
Tirzaan Neil <Tirzaan@tntneil.com>
2025-09-01 21:52:43 -0600

    [Episode] 16 code

---
+++ SwiftfulCrypto/Utilities/HapticManager.swift
@@ -0,0 +1,17 @@
+//
+//  HapticManager.swift
+//  SwiftfulCrypto
+//
+//  Created by Tirzaan on 9/1/25.
+//
+
+import Foundation
+import SwiftUI
+
+class HapticManager {
+    static private let generator = UINotificationFeedbackGenerator()
+    
+    static func notification(type: UINotificationFeedbackGenerator.FeedbackType) {
+        generator.notificationOccurred(type)
+    }
+}
